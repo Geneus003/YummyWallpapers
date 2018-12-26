06d1dfe5b8f7c219e3f4871c447aed7bc7286d21
geneus003 <geneus003@gmail.com>
2018-12-26 21:27:26 +0300

    It works but need to check. Checking in progress

--- check_update_and_set.py
+++ check_update_and_set.py
@@ -1,12 +1,27 @@
-import os, sys
+import os, sys, requests
+
+def download_image_by_url(photo_url):
+
+    p = requests.get(photo_url)
+    out = open("/home/geneus/Documents/YummyWallpapers/wallpaper.jpg", "wb")
+    out.write(p.content)
+    out.close()
+
+    os.system("/usr/bin/gsettings set org.gnome.desktop.background picture-uri /home/geneus/Documents/YummyWallpapers/wallpaper.jpg")
+
 
 def set_image_to_screen(image_date, file_url):
+    print(image_date, file_url)
 
+    wall_info = open("/home/geneus/Documents/YummyWallpapers/last.txt", "w")
+    wall_info.write(image_date)
+    wall_info.close()
 
+    download_image_by_url(file_url)
 
 def check_for_updates(list_of_last_images):
 
-    print(list_of_last_images)
+    # print(list_of_last_images)
 
     if not(os.path.exists("/home/geneus/Documents/YummyWallpapers/last.txt")):
         wall_info = open("/home/geneus/Documents/YummyWallpapers/last.txt", "w")
@@ -15,16 +30,23 @@ def check_for_updates(list_of_last_images):
     else:
         wall_info = open("/home/geneus/Documents/YummyWallpapers/last.txt", "r")
         wall_info = wall_info.read()
-        wall_info = wall_info[:-1]
 
-        print(wall_info)
-        print(str(list_of_last_images[0]["date"]))
+        # print(wall_info)
+        # print(str(list_of_last_images[0]["date"]))
 
         print(len(wall_info), len(str(list_of_last_images[0]["date"])))
 
+        # print(list_of_last_images[0]["attachments"][0]["photo"]["sizes"])
 
         if wall_info != str(list_of_last_images[0]["date"]):
-            set_image_to_screen(str(list_of_last_images[0]["date"])
+
+            for i in range(len(list_of_last_images[0]["attachments"][0]["photo"]["sizes"])):
+                # print(list_of_last_images[0]["attachments"][0]["photo"]["sizes"][i])
+                if list_of_last_images[0]["attachments"][0]["photo"]["sizes"][i]["width"] == 1920:
+                    file_url_1920 = list_of_last_images[0]["attachments"][0]["photo"]["sizes"][i]["url"]
+                    # print("GOD")
+
+            set_image_to_screen(str(list_of_last_images[0]["date"]), file_url_1920)
         else:
             print("Nothing interesting")
     return
--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import get_list_of_images
 import check_update_and_set
 
+import time
 
 def main():
     vk_login, vk_pass = input().split(" ")
+    while 1:
 
-    feed_list = get_list_of_images.get_images_list(vk_login, vk_pass)
+        feed_list = get_list_of_images.get_images_list(vk_login, vk_pass)
 
-    check_update_and_set.check_for_updates(feed_list)
+        check_update_and_set.check_for_updates(feed_list)
+
+        time.sleep(3600)
 
 
 
